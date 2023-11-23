99534e6effece50515b250f3622c281ae2b7bef7
Jean Luc <jlahoyo@asin.bj>
2023-11-23 17:10:39 +0100

    feat: change refresh button action

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-
-const randomNumber1 = Math.floor(Math.random() * 6) + 1;
-const diceImageFilename1 = `images/dice${randomNumber1}.png`;
-const leftImage = document.querySelector('.img1');
-leftImage.setAttribute('src', diceImageFilename1);
-
 function check_win(nb1, nb2) {
     if (nb1 > nb2)
         return 1;
@@ -13,26 +7,37 @@ function check_win(nb1, nb2) {
         return 0;
 }
 
-const rightImage = document.querySelector('.img2');
-rightImage.classList.add('rotate');
+function refreshDice() {
+    const leftImage = document.querySelector('.img1');
+    const rightImage = document.querySelector('.img2');
+    const text = document.querySelector('h1');
 
-setTimeout(() => {
+    // Generate new random numbers for both dice
+    const randomNumber1 = Math.floor(Math.random() * 6) + 1;
+    const diceImageFilename1 = `images/dice${randomNumber1}.png`;
+    leftImage.setAttribute('src', diceImageFilename1);
+
     const randomNumber2 = Math.floor(Math.random() * 6) + 1;
     const diceImageFilename2 = `images/dice${randomNumber2}.png`;
-    rightImage.classList.remove('rotate');
-    rightImage.setAttribute('src', diceImageFilename2);
-    const checker = check_win(randomNumber1, randomNumber2);
-    const text = document.querySelector('h1');
-    
-    if (checker === 1)
-        text.innerHTML = "Player WIN !";
-    else if (checker === 2)
-        text.innerHTML = "CPU WIN ! You're so Bad...";
-    else
-        text.innerHTML = "Draw !";
-}, 2000);
+    rightImage.classList.add('rotate');
 
-function refreshPage() {
-    // document.getElementById('clickSound').play();
-    location.reload(true);
+    // Wait for a short duration before revealing the second dice to simulate rotation
+    setTimeout(() => {
+        rightImage.classList.remove('rotate');
+        rightImage.setAttribute('src', diceImageFilename2);
+
+        // Check the winner
+        const checker = check_win(randomNumber1, randomNumber2);
+
+        // Update the result text
+        if (checker === 1)
+            text.innerHTML = "Player WIN !";
+        else if (checker === 2)
+            text.innerHTML = "CPU WIN ! You're so Bad...";
+        else
+            text.innerHTML = "Draw !";
+    }, 1000); // Adjust the duration based on your preference
 }
+
+// Call refreshDice() when the button is clicked
+document.querySelector('.refresh-button').addEventListener('click', refreshDice);
